718dfab031a8516f362370b33b28dff4c1e16536
stanley <houyuqing@hotmail.com>
2025-04-17 13:32:44 +0800

    add logger module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { createWinstonLogger, WinstonLogger } from "./lib/common.logger";
+export { LoggerModule } from "./lib/logger.module";
 export { RequestExceptionFilter } from "./lib/request.exception.filter";
 export { RequestLoggerInterceptor } from "./lib/request.logger.interceptor";
 
