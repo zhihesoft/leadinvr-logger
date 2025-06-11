89a6172e169d6ef5b169ad3b7654861e91ac1af4
stanley <houyuqing@hotmail.com>
2025-06-11 14:33:26 +0800

    use normal module defination

--- src/lib/logger.module.ts
+++ src/lib/logger.module.ts
@@ -1,25 +1,13 @@
-import { DynamicModule, Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
 import { RequestExceptionFilter } from "./request.exception.filter";
 import { RequestLoggerInterceptor } from "./request.logger.interceptor";
 
+@Global()
 @Module({
     providers: [
         { provide: APP_INTERCEPTOR, useClass: RequestLoggerInterceptor },
         { provide: APP_FILTER, useClass: RequestExceptionFilter },
     ],
 })
-export class LoggerModule {
-    static register(autoRegister: boolean): DynamicModule {
-        return {
-            module: LoggerModule,
-            providers: autoRegister
-                ? [
-                      { provide: APP_INTERCEPTOR, useClass: RequestLoggerInterceptor },
-                      { provide: APP_FILTER, useClass: RequestExceptionFilter },
-                  ]
-                : [],
-            exports: [],
-        };
-    }
-}
+export class LoggerModule {}
